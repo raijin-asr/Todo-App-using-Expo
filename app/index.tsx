1cf933a8328bd13421fd261ed889401b14567f5e
Ameer Sampang Rai <97660344+raijin-asr@users.noreply.github.com>
2025-05-08 11:00:54 +0545

    Day 3: Create Add button, input form, todo item FlatList and Add todo functionality

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,18 @@
-
-import { FlatList, Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { FlatList, Image, KeyboardAvoidingView, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 import {Ionicons} from '@expo/vector-icons';
 import { Checkbox } from 'expo-checkbox';
+import { useState } from "react";
+
+// custom types for todo items
+type TodoType = {
+  id: number;
+  title: string;
+  isDone: boolean;
+}
 
 export default function Index() {
+  // Sample data for todo items
   const todoData= [
     {
       id:1,
@@ -26,7 +34,22 @@ export default function Index() {
       title: "Todo 4",
       isDone: false,
     },
-  ]
+  ];
+
+  const [todos, setTodos] = useState<TodoType[]>(todoData);
+  const [todoText, setTodoText] = useState<string>("");
+
+  // Function to add a new todo item
+  const addTodo = () => {
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoText,
+      isDone: false,
+    };
+    todos.push(newTodo);
+    setTodos(todos);
+    setTodoText("");
+  }
   
   return (
     <SafeAreaView style={styles.container}>
@@ -46,21 +69,52 @@ export default function Index() {
           clearButtonMode="always"
         />
       </View>
-      <FlatList data={todoData} keyExtractor={(item) => item.id.toString()} 
+      <FlatList data={[...todos].reverse()} keyExtractor={(item) => item.id.toString()} 
       renderItem={({item}) => (
-        <View style={styles.todoContainer}>
-          <View style={styles.todoItem}>
-            <Checkbox
-              value={item.isDone}
-              />
-            <Text>{item.title}</Text>
-          </View>
-        </View>
+        <TodoItem todo={item} />
       )} />
 
+      <KeyboardAvoidingView style={styles.addTodoContainer} behavior="padding" keyboardVerticalOffset={15}>
+        <TextInput 
+          placeholder="Add New Todo Task"
+          value={todoText}
+          onChangeText={(text) => setTodoText(text)} 
+          style={ styles.addInput}
+          autoCorrect={false}
+        />
+        <TouchableOpacity style={styles.addButton} onPress={() => addTodo()}>
+          <Ionicons name="add" size={32} color={'white'}/>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
     </SafeAreaView>
   );
 }
+
+//separate component for todo item
+const TodoItem = ({todo}: {todo: TodoType}) => {
+  return (
+    <View style={styles.todoContainer}>
+          <View style={styles.todoItem}>
+            <Checkbox
+              value={todo.isDone}
+              color={todo.isDone ? 'green' : undefined}
+              />
+            <Text 
+              style={[styles.todoText, todo.isDone && {textDecorationLine: 'line-through'} ]}
+            >
+              {todo.title}</Text>
+          </View>
+          <View>
+            <TouchableOpacity 
+              onPress={() => {alert("Deleted "+ todo.title)}}
+            >
+            <Ionicons name="trash" size={24} color={'red'}/>
+            </TouchableOpacity>
+          </View>
+        </View>
+  )
+}
+
 
 const styles= StyleSheet.create({
 container:{
@@ -73,6 +127,7 @@ header:{
   justifyContent:'space-between',
   flexDirection: 'row',
   alignItems: 'center',
+  marginTop: 15,
 },
 searchBar:{
   backgroundColor: 'white',
@@ -89,8 +144,10 @@ searchInput:{
   color: 'black',
 },
 todoContainer:{
+  flexDirection: 'row',
+  justifyContent: 'space-between',
   backgroundColor: 'white',
-  padding: 10,
+  padding: 14,
   borderRadius: 10,
   marginBottom: 10,
 },
@@ -98,5 +155,30 @@ todoItem:{
   flexDirection: 'row',
   alignItems: 'center',
   gap: 10,
+},
+todoText:{
+  fontSize: 16,
+  color: 'black',
+},
+addTodoContainer:{
+  flexDirection: 'row',
+  alignItems: 'center',
+  justifyContent: 'space-between',
+  marginBottom: 20,
+
+},
+addInput:{
+  flex: 1,
+  fontSize: 16,
+  color: 'black',
+  padding: 16,
+  borderRadius: 10,
+  backgroundColor: 'white',
+},
+addButton:{
+  backgroundColor: 'green',
+  padding: 9,
+  borderRadius: 10,
+  marginLeft: 10,
 },
 });
